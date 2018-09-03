34e2349bd50e250f1297e69a812a00ee495782e5
Terrance <terrance.hanlon@ndsu.edu>
2018-09-03 12:59:57 -0500

    show secret tools

---
+++ secret_tools.js
@@ -0,0 +1,12 @@
+module.exports = {
+    conjureState: function(length){
+        var text = '';
+        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      
+        for (var i = 0; i < length; i++) {
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+        console.log(text);
+        return text;
+    }
+}
