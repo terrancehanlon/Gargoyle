70f58c5e00dc6eef0d55d60817e74fd1cd9b51dc
Terrance <terrance.hanlon@ndsu.edu>
2018-09-30 21:49:01 -0500

    server

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.get('/main', (req, res) => {
 	console.log(__dirname);
 })
 
-app.get('/frontend', function(req, res) {
+app.get('/', function(req, res) {
 	console.log(__dirname);
 	res.sendFile(__dirname + '/frontend/dist/frontend/index.html');
 })
